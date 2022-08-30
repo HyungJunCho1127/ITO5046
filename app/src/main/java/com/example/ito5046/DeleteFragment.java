e8b5b144cffa27d7f162763858e5f7c97a5c4a08
davch <hyungjuncho27@gmail.com>
2022-08-30 16:34:57 +0900

    fixed android:inputType="textPassword"

--- app/src/main/java/com/example/ito5046/DeleteFragment.java
+++ app/src/main/java/com/example/ito5046/DeleteFragment.java
@@ -1,11 +1,13 @@
 package com.example.ito5046;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +16,7 @@ import androidx.fragment.app.Fragment;
 public class DeleteFragment extends Fragment {
     private TextView delete_battle_name;
     private Button yes,no;
+    private Competition competition;
 
     @Nullable
     @Override
@@ -24,7 +27,37 @@ public class DeleteFragment extends Fragment {
         no = view.findViewById(R.id.no_delete_button);
         delete_battle_name.setText(Singleton.getBattle());
 
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        competition = Database.getINSTANCE(getContext()).competitionDao().findCompetitionName(Singleton.getBattle());
+                        System.out.println(competition.toString());
+                        Database.getINSTANCE(getContext()).competitionDao().deleteCompetition(competition);
+                    }
+                });
+                thread.start();
+                showToast("Competition Deleted");
+                Intent intent = new Intent(getContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container2, new SelectCompetitionFragment()).commit();
+            }
+        });
+
 
         return view;
+    }
+
+    public void showToast(final String toast)
+    {
+        getActivity().runOnUiThread(() -> Toast.makeText(getContext(), toast, Toast.LENGTH_SHORT).show());
     }
 }
